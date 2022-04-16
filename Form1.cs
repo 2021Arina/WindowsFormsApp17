e563a1b15ff21729ab531e930dd213e0b1b77bd1
2021Arina <93840292+2021Arina@users.noreply.github.com>
2022-04-16 19:54:24 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿
+// 1. Разработать программу, динамически порождающую на окне
+// кнопки.Левый верхний угол кнопки определяется местоположением
+// курсора при щелчке. Вывести надпись на кнопке с координатами ее левого верхнего угла. 
+
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left || e.Button == MouseButtons.Right)
+            {
+                Button b = new Button();
+                b.Parent = this;
+                b.Location = new Point(e.X, e.Y);
+                foreach (Control c in this.Controls)
+                    if (b is Button) 
+                    (b as Button).Text = string.Format("{0}, {1}", e.X, e.Y);
+               // b.Text = string.Format("{0}, {1}", e.X, e.Y);
+
+            }
+        }
+
+    }
+    
+}
